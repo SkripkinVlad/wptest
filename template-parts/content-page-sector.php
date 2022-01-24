e75f315a1ffca433fc71a60ed81d1ca7e48e5555
SkripkinVlad <vladislav.skripkin.devmrks@gmail.com>
2022-01-24 15:36:11 +0200

    Template for Sector upd

--- template-parts/content-page-sector.php
+++ template-parts/content-page-sector.php
@@ -11,6 +11,99 @@
                 <h4><?php the_field( 'sector_quotes_text' ); ?></h4>
                 <p style="color:<?php the_field( 'primary_sector_colour' ); ?>"><?php the_field( 'sector_quotes_text_author' ); ?></p>
             </div>
+        </div>
+
+
+        <div class="row btn-group">
+            <div class="col-2">
+                <a href="<?php the_field( 'sector_btn_link_1' ); ?>" class="btn-main btn-left" style="border-color:<?php the_field( 'primary_sector_colour' ); ?>">
+                    <?php the_field( 'sector_button_1' ); ?>
+                </a>
+            </div>
+            <div class="col-2">
+                <a href="<?php the_field( 'sector_btn_link_2' ); ?>" class="btn-main btn-right" style="border-color:<?php the_field( 'primary_sector_colour' ); ?>">
+                    <?php the_field( 'sector_button_2' ); ?>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="sector__body" style="background: #000 url(<?php the_field( 'sector_body_bg' ); ?>) 0 0 no-repeat; background-size: cover;">
+    <div class="container">
+        <?php the_field( 'main_text_sector' ); ?>
+
+        <div class="row">
+            <?php if ( have_rows( 'sector_service_1' ) ) : ?>
+                <?php while ( have_rows( 'sector_service_1' ) ) : the_row(); ?>
+                    <div class="col-5">
+                        <div class="sector-service-item">
+                            <?php if ( get_sub_field( 'icon_1' ) ) : ?>
+                                <img src="<?php the_sub_field( 'icon_1' ); ?>" />
+                            <?php endif ?>
+                            <?php the_sub_field( 'title_1' ); ?>
+                            <?php the_sub_field( 'description_1' ); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+            <?php if ( have_rows( 'sector_service_2' ) ) : ?>
+                <?php while ( have_rows( 'sector_service_2' ) ) : the_row(); ?>
+                    <div class="col-5">
+                        <div class="sector-service-item">
+                            <?php if ( get_sub_field( 'icon_1' ) ) : ?>
+                                <img src="<?php the_sub_field( 'icon_1' ); ?>" />
+                            <?php endif ?>
+                            <?php the_sub_field( 'title_1' ); ?>
+                            <?php the_sub_field( 'description_1' ); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+            <?php if ( have_rows( 'sector_service_3' ) ) : ?>
+                <?php while ( have_rows( 'sector_service_3' ) ) : the_row(); ?>
+                    <div class="col-5">
+                        <div class="sector-service-item">
+                            <?php if ( get_sub_field( 'icon_1' ) ) : ?>
+                                <img src="<?php the_sub_field( 'icon_1' ); ?>" />
+                            <?php endif ?>
+                            <?php the_sub_field( 'title_1' ); ?>
+                            <?php the_sub_field( 'description_1' ); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+            <?php if ( have_rows( 'sector_service_4' ) ) : ?>
+                <?php while ( have_rows( 'sector_service_4' ) ) : the_row(); ?>
+                    <div class="col-5">
+                        <div class="sector-service-item">
+                            <?php if ( get_sub_field( 'icon_1' ) ) : ?>
+                                <img src="<?php the_sub_field( 'icon_1' ); ?>" />
+                            <?php endif ?>
+                            <?php the_sub_field( 'title_1' ); ?>
+                            <?php the_sub_field( 'description_1' ); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+            <?php if ( have_rows( 'sector_service_5' ) ) : ?>
+                <?php while ( have_rows( 'sector_service_5' ) ) : the_row(); ?>
+                    <div class="col-5">
+                        <div class="sector-service-item">
+                            <?php if ( get_sub_field( 'icon_1' ) ) : ?>
+                                <img src="<?php the_sub_field( 'icon_1' ); ?>" />
+                            <?php endif ?>
+                            <?php the_sub_field( 'title_1' ); ?>
+                            <?php the_sub_field( 'description_1' ); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
 
         </div>
     </div>
